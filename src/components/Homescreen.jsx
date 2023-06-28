6e96ae3c653e75ef8752a7a1468defd71ca171e0
Soumyadeep Rakshit <111779423+soum-rakshit@users.noreply.github.com>
2023-06-28 14:42:18 +0530

    Initial commit

---
+++ src/components/Homescreen.jsx
@@ -0,0 +1,7 @@
+export default function Homescreen(){
+    return (
+        <section className="Home">
+        Home
+        </section>
+    );
+};
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCLdu7EshspdANJe_RS1INMqmxrBVF6c20",
+    authDomain: "net-clone-3bdae.firebaseapp.com",
+    projectId: "net-clone-3bdae",
+    storageBucket: "net-clone-3bdae.appspot.com",
+    messagingSenderId: "278942174193",
+    appId: "1:278942174193:web:81a2ab7fd85d1326041250",
+    measurementId: "G-QRCJ5JVV8E"
+  };
